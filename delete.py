2a2f45c550a2fe42852c6aa62697d88f97711aca
fogdingding <fogdingding@gmail.com>
2019-12-06 01:18:45 +0800

    for fix db_modle

--- delete.py
+++ delete.py
@@ -9,7 +9,8 @@ def delete(text_id):
     if(len(ret['recs'])>1):
         print('more than one rid')
         return {"Failed","Primary Key more than one"}
-    res = gp.Del(db_name,[rid])
+    # res = gp.Del(db_name,[rid])
+    res = gp.Update(db=db_name,rid=rid,new_record={'confirm':0},modify_all=False,record_format='json')
     return res
     
-# print(delete('cb981edc-16c1-11ea-ba07-2c4d54c22a5f'))
+#print(delete('cb981edc-16c1-11ea-ba07-2c4d54c22a5f'))
