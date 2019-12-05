ffbbfaa17749b14c5e714b5d8fdb0a1a5374339c
fogdingding <fogdingding@gmail.com>
2019-12-06 01:49:07 +0800

    for delete cash

--- app.py
+++ app.py
@@ -5,14 +5,27 @@ from tags_search import tags_search
 from submit import submit
 from init import init
 from init import get_document
-
+import os
 import json
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path, endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
 
 @app.route("/")
+@cross_origin()
 def hello():
     return render_template('index.html')
 
@@ -58,4 +71,4 @@ def get_document_ca():
     return get_document_data
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="127.0.0.1",port=5000)
